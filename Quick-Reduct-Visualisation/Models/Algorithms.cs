3ab401e2113596e68ab33fe929ffd91dbe59ec9e
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-06-09 21:44:01 +0200

    Initial commit

---
+++ Quick-Reduct-Visualisation/Models/Algorithms.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick_Reduct_Visualisation.Models
+{
+    public class Algorithms
+    {        
+        public Data data = new();
+        public void SetDifferenceTable()
+        {
+            for (int i = 0; i <= data.dataSets.Count; i++)
+            { 
+
+                for(int j=0;j<=data.dataSets[i].Count;j++)
+                {
+                    for(int k=i; k <= data.dataSets.Count;k++)
+                    {
+                        if(data.dataSets[i][j] != data.dataSets[k][j])
+                        {
+                            data.differenceTable[i][k].Add(data.attributes[j]);
+                            data.differenceTable[k][i].Add(data.attributes[j]);
+                        }
+                    }
+                }
+            }
+        }
+        public void CountDifferences()
+        {
+            for (int i = 0; i <= data.differenceTable.Count(); i++)
+            {
+
+                for (int j = 0; j <= data.differenceTable[i].Count(); j++)
+                {
+                    for (int k = i; k <= data.differenceTable[i][j].Count; k++)
+                    {
+                        data.differencesCount[data.differenceTable[i][j][k]]++;
+                    }
+                }
+            }
+        }
+    }
+}
+/*
+    "twardość", "kolor", "wielkość", "dec" 
+    "miękkie", "fioletowe", "duże", "dojrzałe" 
+    "miękkie", "żółte", "duże", "dojrzałe"
+    "miękkie", "zielone", "średnie", "dojrzałe"
+    "miękkie", "zielone", "średnie", "niedojrzałe"
+    "średnie", "fioletowe", "duże", "dojrzałe"
+    "średnie", "żółte", "małe", "dojrzałe"
+    "średnie", "żółte", "małe", "niedojrzałe"
+    "średnie", "zielone", "średnie", "niedojrzałe"
+    "twarde", "fioletowe", "średnie", "dojrzałe"
+    "twarde", "żółte", "małe", "dojrzałe"
+    "twarde", "żółte", "małe", "niedojrzałe"
+    "twarde", "zielone", "duże", "niedojrzałe"
+*/
---
+++ Quick-Reduct-Visualisation/Models/Data.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick_Reduct_Visualisation.Models
+{
+    public class Data
+    {
+        public List<string> attributes =  new List<string>  { "twardość", "kolor", "wielkość", "dec" };
+        public List<List<string>> dataSets = new List<List<string>>
+        {
+            new List<string>{"miękkie", "fioletowe", "duże", "dojrzałe"},
+            new List<string>{"miękkie", "żółte", "duże", "dojrzałe"},
+            new List<string>{"miękkie", "zielone", "średnie", "dojrzałe"},
+            new List<string>{"miękkie", "zielone", "średnie", "niedojrzałe"},
+            new List<string>{"średnie", "fioletowe", "duże", "dojrzałe"},
+            new List<string>{"średnie", "żółte", "małe", "dojrzałe"},
+            new List<string>{"średnie", "żółte", "małe", "niedojrzałe"},
+            new List<string>{"średnie", "zielone", "średnie", "niedojrzałe"},
+            new List<string>{"twarde", "fioletowe", "średnie", "dojrzałe"},
+            new List<string>{"twarde", "żółte", "małe", "dojrzałe"},
+            new List<string>{"twarde", "żółte", "małe", "niedojrzałe"},
+            new List<string>{"twarde", "zielone", "duże", "niedojrzałe"}
+        };
+        //public List<string>[][] 
+        public List<List<List<string>>> differenceTable = new();
+        public Dictionary<string, int> differencesCount = new();
+        private string filePath;
+
+        private void GetFilePath()
+        {
+            // Create open file dialog
+            Microsoft.Win32.OpenFileDialog fileDialog = new();
+
+            // Set filter for file extension and default file extension
+            fileDialog.DefaultExt = ".txt";
+            fileDialog.Filter = "txt files (*.txt)|*.txt";
+
+            // Open File Dialog
+            bool? result = fileDialog.ShowDialog();
+
+            // Get selected file name
+            if (result == true)
+            {
+                filePath = fileDialog.FileName;
+            }
+        }
+
+        public void GetData()
+        {
+            GetFilePath();
+            using(var reader = new StreamReader(filePath))
+            {
+
+            }
+
+        }
+    }
+}
